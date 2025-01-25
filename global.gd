67edd7ebfcf18e85c591c2cf1618cf68dfb9fbc4
Brandon S <116681127+bsibb22@users.noreply.github.com>
2025-01-25 11:22:13 -0500

    Started card script

---
+++ global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var IMAGE_BANK = {
+	"HEART_A": load("res://Sprites/ace.png"),
+	"CARDBACK": load("res://Sprites/cardback.png")
+}
+
+func load_img(img_key: String) -> Sprite2D:
+	if IMAGE_BANK[img_key]:
+		return IMAGE_BANK[img_key]
+	else:
+		print("ERROR: no image found with key %s!" % img_key)
+		return Sprite2D.new()
+
+var value_bank = ['$', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
