2a8f4fbee2f9399259048f26a0235382b4616cf9
shaynemarusic <92888014+shaynemarusic@users.noreply.github.com>
2025-01-25 15:29:08 -0500

    Players can connect with eachother
    Trouble syncing players

--- MultiplayerController.gd
+++ MultiplayerController.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var Address = "127.0.0.1"
 @export var port = 8910
+var peer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,31 +18,57 @@ func _process(delta: float) -> void:
 	pass
 
 func player_connected(id) -> void:
-	print("Player Connected " + id)
+	print("Player Connected " + str(id))
 	
 func player_disconnected(id) -> void:
 	print("Player Disconnected " + id)
 	
-func connected_to_server(id) -> void:
-	print("Connected to Server " + id)
+func connected_to_server() -> void:
+	print("Connected to Server")
+	SendPlayerInformation.rpc_id(1, $LineEdit.text, multiplayer.get_unique_id())	
 	
-func connection_failed(id) -> void:
-	print("Connection Failed " + id)
+func connection_failed() -> void:
+	print("Connection Failed")
+	
+@rpc("any_peer", "call_local")
+func StartGame() -> void:
+	var scene = load("res://test_scene.tscn").instantiate()
+	get_tree().root.add_child(scene)
+	self.hide()
+	
+@rpc("any_peer")
+func SendPlayerInformation(name, id):
+	if !GameManager.Players.has(id):
+		GameManager.Players[id] = {
+			"name" : name,
+			"id" : id
+		}
+		
+	if multiplayer.is_server():
+		for i in GameManager.Players:
+			SendPlayerInformation.rpc(GameManager.Players[i].name, i)
  
 func _on_host_button_down() -> void:
-	var peer = ENetMultiplayerPeer.new()
+	peer = ENetMultiplayerPeer.new()
 	var error = peer.create_server(port, 8)
 	if error != OK:
 		print("Cannot host: " + error)
 		return
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	multiplayer.set_multiplayer_peer(peer)
+	print("Waiting for Players")
+	SendPlayerInformation($LineEdit.text, multiplayer.get_unique_id())
 	pass # Replace with function body.
 
 
 func _on_join_button_down() -> void:
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(Address, port)
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(peer)
 	pass # Replace with function body.
 
 
 func _on_start_button_down() -> void:
+	StartGame.rpc()
 	pass # Replace with function body.
