f585eb2d24f3e31d30e946119aa5ba57c9f14314
jackhernandez23 <jack.hernandez@ufl.edu>
2025-01-25 18:17:08 -0500

    added main menu functionality

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -7,3 +7,8 @@ func _on_play_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
