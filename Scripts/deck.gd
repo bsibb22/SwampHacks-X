c753e6d7a98e9f8b10bb0424004f25cf8b2873dc
Brandon S <116681127+bsibb22@users.noreply.github.com>
2025-01-25 18:24:55 -0500

    added basic turn system

--- Scripts/deck.gd
+++ Scripts/deck.gd
@@ -13,5 +13,5 @@ func _process(delta: float) -> void:
 # func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-	# if(player.is_turn):
-		get_parent().deal_card(0, 1)
+		if global.pid == get_node("../").player_ids[get_node("../").turn_counter]:
+			get_parent().deal_card(0, 1)
