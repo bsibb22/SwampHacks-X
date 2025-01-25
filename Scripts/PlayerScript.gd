18a38064a2428ffff1360ad62335ed29c2773123
Brandon S <116681127+bsibb22@users.noreply.github.com>
2025-01-25 17:51:26 -0500

    spring cleaning

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -14,11 +14,10 @@ func update_hand():
 		c.owner = global.pid
 		var cs = card_scene.instantiate()
 		cs.data = c
-		cs.rotation = deg_to_rad(randf_range(-10, 10))
 		add_child(cs)
 
 	for i in range(get_child_count()):
 		get_child(i).position = Vector2(-180 + floor(360 / (get_child_count())) * i, 0)
-		
+
 func _ready() -> void:
 	get_parent().update.connect(update_hand)
