8f30a2cabccb83ec001ed657a2967a4ebf151483
Brandon S <116681127+bsibb22@users.noreply.github.com>
2025-01-25 12:15:37 -0500

    Added shuffling logic

--- global.gd
+++ global.gd
@@ -12,9 +12,34 @@ func load_img(img_key: String) -> Sprite2D:
 		print("ERROR: no image found with key %s!" % img_key)
 		return Sprite2D.new()
 
-var value_bank = ['$', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-func _shuffle(arr = []):
-	pass
-	
-func _ready() -> void:
-	
+func shuffle_deck() -> void:
+	# save the most recently played card
+	var temp = pile.pop_back()
+
+	# randomly add cards from the discard pile to the deck
+	while pile.length() > 0:
+		var c = pile.pick_random()
+		if (c == temp):
+			continue
+		c.owner = 8
+		deck.push(c)
+
+var deck = [] # stack
+var players = [] # 2d player array
+var pile = []
+
+func load_game(num_players: int) -> void:
+	# Create the pile of cards
+	for i in range(0, 54):
+	# make new card with id i and owner pile
+		var c: CardData = CardData.new(i)
+		deck.push(c)
+
+	# Shuffle the deck
+	deck.shuffle()
+
+	# Deal the cards
+	for i in range(0, num_players):
+		players[i] = []
+		for j in range(0, 3):
+			players[i].push(deck.pop_back())
