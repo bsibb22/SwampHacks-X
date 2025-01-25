f5bf24bf4be4cecbb64dcb5589e9e3172cc9c7fb
jackhernandez23 <jack.hernandez@ufl.edu>
2025-01-25 18:14:31 -0500

    added main menu functionality

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,3 +3,7 @@ extends Node
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 	
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
