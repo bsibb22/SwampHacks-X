74f8ed726607046d0e5f2b9ae7564cc63bd4108c
jackhernandez23 <jack.hernandez@ufl.edu>
2025-01-25 12:19:55 -0500

    starting work

--- Scripts/CardScript.gd
+++ Scripts/CardScript.gd
@@ -1,19 +1,18 @@
 extends Sprite2D
 
-@export_range(0, 13) var card_value: int = 1
-@export_enum("HEART", "CLUB", "DIAMOND", "SPADE") var suit: String = "HEART"
-var known: bool = false
+@export var data: Resource
 
 @onready var area = $Area2D
 @onready var sprite = $Sprite2D
 
 func _ready() -> void:
-	pass
+  sprite.texture = global.load_img(data.suit + "_" + data.card_value)
+  pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+  pass
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	print_rich()
-	pass # Replace with function body.
+  print_rich(data.card_value + ", " + data.card_suit)
+  pass # Replace with function body.
--- global.gd
+++ global.gd
@@ -1,8 +1,8 @@
 extends Node
 
 @onready var IMAGE_BANK = {
-	"HEART_A": load("res://Sprites/ace.png"),
-	"CARDBACK": load("res://Sprites/cardback.png")
+  "HEART_A": load("res://Sprites/ace.png"),
+  "CARDBACK": load("res://Sprites/cardback.png")
 }
 
 func load_img(img_key: String) -> Sprite2D:
@@ -12,4 +12,38 @@ func load_img(img_key: String) -> Sprite2D:
 		print("ERROR: no image found with key %s!" % img_key)
 		return Sprite2D.new()
 
-var value_bank = ['$', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+func shuffle_deck() -> void:
+  # save the most recently played card
+	var temp = pile.pop_back()
+
+	 # randomly add cards from the discard pile to the deck
+	while pile.length() > 0:
+		var c = pile.pick_random()
+		if (c == temp):
+			continue
+		c.owner = 8
+		deck.push(c)
+	
+	pile.push(temp)
+
+# ---- #
+
+var deck = [] # stack
+var players = [] # 2d player array
+var pile = []
+
+func load_game(num_players: int) -> void:
+	# Create the pile of cards
+	for i in range(0, 54): 
+		# make new card with id i and owner pile
+		var c: CardData = CardData.new(i)
+		deck.push(c)
+
+	# Shuffle the deck
+	deck.shuffle()
+
+	# Deal the cards
+	for i in range(0, num_players):
+		players[i] = []
+		for j in range(0, 3):
+			players[i].push(deck.pop_back())
